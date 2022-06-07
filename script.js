2b57028060ec9565d49a28c63fe466e4a80c518b
Gokulcasper <ronitcasper215@gmail.com>
2022-06-07 12:31:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Filter Elements in Array
+
+const number = [1, 2, 3, 4, 5];
+let even = number.filter(function (value) {
+  return value % 2 == 0;
+});
+let odd = number.filter(function (value) {
+  return value % 2 == 1;
+});
+console.log("Even Numbers are : ", even);
+console.log("odd Numbers are : ", odd);
+
+// Filter Elements in Array Objects
+
+const cart_item = [
+  { id: 1, itemName: "iphone", cost: "30000" },
+  { id: 1, itemName: "microsoft mobile", cost: "6000" },
+  { id: 1, itemName: "android mobile", cost: "10000" },
+];
+let budgetMobile = cart_item.filter(function (value) {
+  return value.cost <= 10000;
+});
+console.log("Affortable Price Mobiles are : ", budgetMobile);
